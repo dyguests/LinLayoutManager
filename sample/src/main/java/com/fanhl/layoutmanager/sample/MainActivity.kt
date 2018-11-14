8ff4912149f70d8db3f32338d8492b1b27a76cdc
fanhl <fanhonglin@klicen.com>
2018-11-14 14:57:18 +0800

    layout manager

--- sample/src/main/java/com/fanhl/layoutmanager/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/layoutmanager/sample/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.fanhl.layoutmanager.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_view.view.*
 
 class MainActivity : AppCompatActivity() {
+    private val adapter = MainAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        assignViews()
+        initData()
+        refreshData()
+    }
+
+    private fun assignViews() {
+    }
+
+    private fun initData() {
+        recycler_view.adapter = adapter
+    }
+
+    private fun refreshData() {
+        adapter.setNewData(
+            List(10) {
+                "$it"
+            }
+        )
+    }
+
+    class MainAdapter : BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_view) {
+        override fun convert(helper: BaseViewHolder?, item: String?) {
+            helper?.itemView?.apply {
+                tv_1.text = item
+            }
+        }
     }
 }
