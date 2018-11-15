6a7d56a9c14a71ebe1748e2012b8a19a56a6d9ff
fanhl <fanhonglin@klicen.com>
2018-11-15 11:02:52 +0800

    scrollVerticallyBy

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ Resources:
 
 [创建 RecyclerView LayoutManager – Part 3](https://github.com/hehonghui/android-tech-frontier/blob/master/issue-13/%E5%88%9B%E5%BB%BA-RecyclerView-LayoutManager-Part-3.md)
 
-[打造属于你的 LayoutManager](https://juejin.im/entry/57597452a34131006133369e)
+[打造属于你的 LayoutManager](https://www.jianshu.com/p/08d998d047d8)
--- library/src/main/java/com/fanhl/layoutmanager/DemoLayoutManager.kt
+++ library/src/main/java/com/fanhl/layoutmanager/DemoLayoutManager.kt
@@ -3,6 +3,9 @@ package com.fanhl.layoutmanager
 import android.support.v7.widget.RecyclerView
 
 class DemoLayoutManager : RecyclerView.LayoutManager() {
+    private var verticalScrollOffset = 0
+    private var totalHeight = 0
+
     override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
         return RecyclerView.LayoutParams(
             RecyclerView.LayoutParams.WRAP_CONTENT,
@@ -11,9 +14,17 @@ class DemoLayoutManager : RecyclerView.LayoutManager() {
     }
 
     override fun onLayoutChildren(recycler: RecyclerView.Recycler, state: RecyclerView.State) {
+        //如果没有item，直接返回
+        if (itemCount <= 0) return
+        // 跳过preLayout，preLayout主要用于支持动画
+        if (state.isPreLayout) {
+            return
+        }
+
         detachAndScrapAttachedViews(recycler)
 
         var offsetY = 0
+        totalHeight = 0
         for (i in 0 until itemCount) {
 
             val view = recycler.getViewForPosition(i)
@@ -29,11 +40,29 @@ class DemoLayoutManager : RecyclerView.LayoutManager() {
 
             offsetY += height
         }
+
+        //如果所有子View的高度和没有填满RecyclerView的高度，
+        // 则将高度设置为RecyclerView的高度
+        totalHeight = Math.max(totalHeight, getVerticalSpace())
     }
 
     override fun canScrollVertically(): Boolean {
         return true
     }
 
+    override fun scrollVerticallyBy(dy: Int, recycler: RecyclerView.Recycler, state: RecyclerView.State): Int {
+        var travel = dy
 
+        if (verticalScrollOffset + dy < 0) {
+            travel = -verticalScrollOffset
+        } else if (verticalScrollOffset + dy > totalHeight - getVerticalSpace()) {
+            travel = totalHeight - getVerticalSpace() - verticalScrollOffset
+        }
+
+        return travel
+    }
+
+    private fun getVerticalSpace(): Int {
+        return height - paddingBottom - paddingTop
+    }
 }
