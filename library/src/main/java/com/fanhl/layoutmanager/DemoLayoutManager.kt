d15361fcaac96767b744918f52d819f8228db88a
fanhl <fanhonglin@klicen.com>
2018-11-15 10:28:43 +0800

    onLayoutChildren

--- library/src/main/java/com/fanhl/layoutmanager/DemoLayoutManager.kt
+++ library/src/main/java/com/fanhl/layoutmanager/DemoLayoutManager.kt
@@ -9,4 +9,25 @@ class DemoLayoutManager : RecyclerView.LayoutManager() {
             RecyclerView.LayoutParams.WRAP_CONTENT
         )
     }
+
+    override fun onLayoutChildren(recycler: RecyclerView.Recycler, state: RecyclerView.State) {
+        detachAndScrapAttachedViews(recycler)
+
+        var offsetY = 0
+        for (i in 0 until itemCount) {
+
+            val view = recycler.getViewForPosition(i)
+
+            addView(view)
+
+            measureChildWithMargins(view, 0, 0)
+
+            val width = getDecoratedMeasuredWidth(view)
+            val height = getDecoratedMeasuredHeight(view)
+
+            layoutDecorated(view, 0, offsetY, width, offsetY + height)
+
+            offsetY += height
+        }
+    }
 }
